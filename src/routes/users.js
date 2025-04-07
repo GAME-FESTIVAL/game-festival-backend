2cb2f62b2ab18b47de18c029ddb4728f8a85f4ad
yonghwanjung <coxldtpeo1@naver.com>
2025-04-07 22:48:59 +0900

    task(common):유저 리스트 API 추가(auth 미들웨어 주석)

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,14 +6,18 @@ const Game = require("../models/Game");
 const Comment = require("../models/Comment");
 const throwError = require("../utils/throwError");
 
-router.get("/", auth, async (req, res) => {
-  try {
-    const users = await User.find().select("_id nickname");
-    res.status(200).json(users);
-  } catch (err) {
-    next(err);
+router.get(
+  "/",
+  // auth,
+  async (req, res) => {
+    try {
+      const users = await User.find().select("_id nickname");
+      res.status(200).json(users);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 router.get("/info", auth, async (req, res) => {
   return res.json({
